d98ea6653f5d883e89b98f66408b1fe617c38b6f
Raxxius <Raxxman@gmail.com>
2022-02-03 12:00:32 +0000

    add first test

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,20 @@
+const addition = require("../calc")
+
+describe("Calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 22 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+    });
+
+    describe("Subtraction function", () => {
+        
+    });
+
+    describe("Multiplcation function", () => {
+        
+    });
+
+    describe("Division function", () => {
+        
+    });
+})
